d2d43c4bae0ece865812c696bbe2535aaba88756
unknown <harfiantobinjeman@gmail.com>
2023-05-20 03:41:31 +0700

    first commit

---
+++ TestKerja/Controllers/HomeController.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http.Headers;
+using TestKerja.Models;
+
+namespace TestKerja.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly HttpClient httpClient;
+        string Baseurl = "https://reqres.in/";
+
+        public HomeController(ILogger<HomeController> logger, HttpClient httpClient)
+        {
+            _logger = logger;
+            this.httpClient = httpClient;
+        }
+
+        public async Task<ActionResult> Index(string email,string password)
+        {
+            string emails = "eve.holt@reqres.in";
+            string passwords = "cityslicka";
+            string url = "https://reqres.in/api/login";
+            var req = (HttpWebRequest)WebRequest.Create(url);
+            req.Credentials = new NetworkCredential(emails, passwords);
+            var response = req.GetResponse();
+
+            if (response != null)
+            {
+                return RedirectToAction(nameof(GetAll));
+            }
+            else
+            {
+                return View(Index);
+            }
+        }
+
+        public async Task<ActionResult> GetAll(string Sorting_Order, string Search_Data, string Filter_Value, int? Page_No)
+        {
+
+            ViewBag.CurrentSortOrder = Sorting_Order;
+            ViewBag.SortingName = String.IsNullOrEmpty(Sorting_Order) ? "Name_Description" : "";
+            ViewBag.SortingDate = Sorting_Order == "Date_Enroll" ? "Date_Description" : "Date";
+
+            if (Search_Data != null)
+            {
+                Page_No = 1;
+            }
+            else
+            {
+                Search_Data = Filter_Value;
+            }
+
+            ViewBag.FilterValue = Search_Data;
+
+
+
+            var UsersInfo = new Users();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage Res = await client.GetAsync("api/users");
+
+                if (Res.IsSuccessStatusCode)
+                {
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+                    UsersInfo = JsonConvert.DeserializeObject<Users>(EmpResponse);
+
+                }
+
+                var user = from stu in UsersInfo.data select stu;
+
+                if (!String.IsNullOrEmpty(Search_Data))
+                {
+                    user = user.Where(stu => stu.first_name.ToUpper().Contains(Search_Data.ToUpper())
+                        || stu.last_name.ToUpper().Contains(Search_Data.ToUpper()));
+                }
+                switch (Sorting_Order)
+                {
+                    case "Name_Description":
+                        user = user.OrderByDescending(stu => stu.last_name);
+                        break;
+                    case "Date_Enroll":
+                        user = user.OrderBy(stu => stu.email);
+                        break;
+                    case "Date_Description":
+                        user = user.OrderByDescending(stu => stu.email);
+                        break;
+                    default:
+                        user = user.OrderBy(stu => stu.first_name);
+                        break;
+                }
+
+                int Size_Of_Page = 4;
+                int No_Of_Page = (Page_No ?? 1);
+                return View(UsersInfo);
+            }
+        }
+
+        /*        public async Task<IEnumerable<Users>> GetAllUser()
+                {
+                    {
+                        try
+                        {
+                            var user = await this.httpClient.GetFromJsonAsync<IEnumerable<Users>>("https://reqres.in/api/users?page=2");
+                            return user;
+                        }
+                        catch (Exception)
+                        {
+
+                            throw;
+                        }
+                    }
+                }*/
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TestKerja/Models/Users.cs
@@ -0,0 +1,19 @@
+﻿namespace TestKerja.Models
+{
+    public class Users
+    {
+        public int page { get; set; }
+        public int per_page { get; set; }
+        public int total { get; set; }
+        public int total_page { get; set; }
+        public List<Data> data { get; set; }
+    }
+    public class Data
+    {
+        public int id { get; set; }
+        public string email { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string avatar { get; set; }
+    }
+}
